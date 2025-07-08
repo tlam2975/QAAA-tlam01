6fbc99df1c9a60ca213a90816d6ebdb455040b7a
tlam2975 <trtunglam@gmail.com>
2025-07-08 16:39:14 +0700

    push 02

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
+function addcl() {
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
-function remcl(){
+function remcl() {
 	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
+	if (this.value == "") {
 		parent.classList.remove("focus");
 	}
 }
@@ -18,3 +18,26 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+function login() {
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	localStorage.setItem("username", username);
+
+	// if (login_status = True) {
+	// 	alert("Login successful!");
+	// 	window.location.href = "main.html"; // Redirect to dashboard
+	// } else {
+	// 	alert("Invalid username or password.");
+	// }
+	if (username == null && password == null) {
+		alert("Please enter username and password.");
+	}
+	else {
+		window.location.href = "/../main.html"; // Redirect to dashboards
+	}
+}
+
+function draft() {
+	console.log("Draft saved");
+}
